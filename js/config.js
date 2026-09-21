64eb0647cfd4ab67ef663ff01e37dc472721ec22
Frank Terpstra <frank@frankterpstra.com>
2026-09-21 13:18:38 +0200

    datum aangepast

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@ let respecConfig = {
 
   //-- publishDate is verplicht. Als je werkversie gekozen hebt  dan pakt Respec
   //-- de pushdate maar de publishDate is nog steeds verplicht.
-  publishDate: "2026-02-09",
+  publishDate: "2026-21-09",
   
   //-- publishVersion is verplicht. Hij mag wel leeg zijn [], maar niet de lege string zijn "".
   publishVersion: [],
